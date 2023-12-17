1c36e2dd11e13191fdb4dbe1e4610ee44981e7b4
wendecoder <wenonezra258@gmail.com>
2023-12-17 11:28:47 +0300

    it was not because of the my configuration

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,7 +2,6 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
-import { CorsOptions } from '@nestjs/common/interfaces/external/cors-options.interface';
 
 @Module({
   imports: [ConfigModule.forRoot()],
--- src/main.ts
+++ src/main.ts
@@ -1,33 +1,16 @@
 import { NestFactory } from '@nestjs/core';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
-import { CorsOptions } from '@nestjs/common/interfaces/external/cors-options.interface';
-
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  // Configure CORS using CorsMiddleware
-  const corsOptions: CorsOptions = {
-    origin: 'https://tokenized-ballot-dapp-repo.vercel.app',
+  app.enableCors({
+    origin: "https://tokenized-ballot-dapp-repo.vercel.app/", // or true to allow any origin
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     allowedHeaders: ['Content-Type'],
     credentials: true,
-  };
-
-  app.use((req, res, next) => {
-    // Use CorsMiddleware
-    // This can also be done with app.enableCors() as a shorthand
-    res.header('Access-Control-Allow-Origin', corsOptions.origin);
-    res.header('Access-Control-Allow-Methods', (corsOptions.methods as string[]).join(', '));
-    res.header('Access-Control-Allow-Headers', (corsOptions.allowedHeaders as string[]).join(', '));
-    res.header('Access-Control-Allow-Credentials', corsOptions.credentials.toString());
-
-    if (req.method === 'OPTIONS') {
-      res.sendStatus(200);
-    } else {
-      next();
-    }
-  });
+  }
+  );
 
   const config = new DocumentBuilder()
     .setTitle('API example')
