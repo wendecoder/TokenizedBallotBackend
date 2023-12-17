1592681f36f6217c8f3bcac3851ebe45327f80b8
wendecoder <wenonezra258@gmail.com>
2023-12-17 11:23:17 +0300

    trying CORS another way

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
+import { CorsOptions } from '@nestjs/common/interfaces/external/cors-options.interface';
 
 @Module({
   imports: [ConfigModule.forRoot()],
