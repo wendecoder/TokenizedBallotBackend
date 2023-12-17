00e9169608a7b5e9b51e6a30352ca0df38e41848
wendecoder <wenonezra258@gmail.com>
2023-12-17 11:02:08 +0300

    trying CORS in different way

--- src/main.ts
+++ src/main.ts
@@ -3,14 +3,14 @@ import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  app.enableCors({
-    origin: "https://tokenized-ballot-dapp-repo.vercel.app/", // or true to allow any origin
-    methods: ['GET', 'POST', 'PUT', 'DELETE'],
-    allowedHeaders: ['Content-Type'],
-    credentials: false,
-  }
-  );
+  const app = await NestFactory.create(AppModule,  { cors: true });
+  // app.enableCors({
+  //   origin: "https://tokenized-ballot-dapp-repo.vercel.app/", // or true to allow any origin
+  //   methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  //   allowedHeaders: ['Content-Type'],
+  //   credentials: false,
+  // }
+  // );
 
   const config = new DocumentBuilder()
     .setTitle('API example')
